fd95cf07fa9c53f5f832b03a6e9740359619fbc3
antonKosenko <anton.kosenko.mk@gmail.comecho>
2020-08-15 21:15:41 +0300

    fix

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models\Http\Middleware;
+namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace App\Models\Providers;
+namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
